a8a18576436f398033e6d1da9d2785aaede41b6d
antoniom28 <antomarc@gmail.com>
2022-03-23 01:05:05 +0100

    riaggiunto i post non pubblicati

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboards')
 
 @section('content')
-   
+    @if ($post->published)
         <div class="card">
             <h1> {{$post->title}} </h1>
             <p> {{$post->content}} </p>
@@ -11,4 +11,10 @@
         <a href="/admin/posts">
             <h3 class="mt-2">Torna ai post</h3>
         </a>
+    @else
+        <h1>POST NON PUBBLICATO</h1>
+        <a href="{{route("admin.posts.index" , ["bozze" => true])}}">
+            <h3 class="mt-2">Torna alle bozze</h3>
+        </a>
+    @endif
 @endsection
